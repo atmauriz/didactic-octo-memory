9aac3b0eee95d9ad044f97d6679a7a739924938e
Maurizio <maurizio.bussi.mb@gmail.com>
2021-04-03 00:11:36 +0200

    setup travis for deployment and continuous testing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# didactic-octo-memory
+# testfolder [![Build Status](https://travis-ci.com/atmauriz/didactic-octo-memory.svg?branch=master)](https://travis-ci.com/atmauriz/didactic-octo-memory) 
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/) [![Github all releases](https://img.shields.io/github/downloads/bmmauri/test-folder/total.svg)](https://GitHub.com/bmmauri/test-folder/releases/)
+
+Test Automation based on Python
+
+Enjoy! Developped with love by @atmauriz
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testfolder-atmauriz", # Replace with your own username
+    version="0.0.1",
+    author="Maurizio Bussi",
+    author_email="maurizio.bussi.mb@gmail.com",
+    description="Python Test Automation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atmauriz/didactic-octo-memory",
+    project_urls={
+        "Bug Tracker": "https://github.com/atmauriz/didactic-octo-memory/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Testing",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "tf"},
+    packages=setuptools.find_packages(where="tf"),
+    python_requires=">=3.7",
+)
