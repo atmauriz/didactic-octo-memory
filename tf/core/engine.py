d8872113c8e3bc97d987733c8feef1a9046c40dd
Maurizio <maurizio.bussi.mb@gmail.com>
2021-04-03 11:10:42 +0200

    add travis telegram notification, add basic test unit

--- tf/core/engine.py
+++ tf/core/engine.py
@@ -1,5 +1,20 @@
-class TestFolder:
+import socketserver
+import threading
+
+
+class TestFolderHandler(socketserver.BaseRequestHandler):
+    """
+    Framework handler
+    """
+
+    def handle(self) -> None:
+        pass
+
+
+class TestFolder(socketserver.ThreadingTCPServer):
     """
     Test Automation object
     """
-    pass
+
+    def __init__(self, server_address=("localhost", 8778), _handler=TestFolderHandler):
+        super().__init__(server_address=server_address, RequestHandlerClass=_handler)
