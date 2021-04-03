1cc66e4263ab50a24bf010948164c86b063a8009
Maurizio <maurizio.bussi.mb@gmail.com>
2021-04-03 11:10:42 +0200

    add travis telegram notification, add basic test unit

--- tests/test_unit_tf.py
+++ tests/test_unit_tf.py
@@ -1,4 +1,5 @@
 import pytest
+import socketserver
 
 from tf.core import engine
 
@@ -6,6 +7,24 @@ from tf.core import engine
 @pytest.mark.unit
 class TestTestFolder:
 
+    tf = None
+
+    @classmethod
+    def setup_class(cls):
+        cls.tf = engine.TestFolder()
+
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        del cls.tf
+
     def test_instance(self):
-        tf = engine.TestFolder()
-        assert isinstance(tf, engine.TestFolder), f"Instance missmatch need to be a {engine.TestFolder.__name__}"
+        assert isinstance(self.tf, engine.TestFolder), f"Instance missmatch need to be a {engine.TestFolder.__name__}"
+
+    def test_sub_instance(self):
+        assert issubclass(self.tf.__class__, socketserver.ThreadingTCPServer), f"Subclass missmatch need to be a {socketserver.ThreadingTCPServer.__name__}"
